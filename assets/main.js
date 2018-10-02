b7bfb2df7b56949b7c221888736a7b34b7969a0d
sumo89 <apple@Henrys-Macbook-Pro.local>
2018-10-02 18:34:30 +0100

    added popup and styling

--- assets/main.js
+++ assets/main.js
@@ -4,4 +4,29 @@ $(document).ready(function(){
         arrows: true,
         slidesToShow: 1
     });
+
+    $(document).on('click', function(event){
+        var target = $(event.target);
+
+        if ( target.parents().hasClass('popups') ){
+            // do nothing
+        }
+        else if ( target.is('.option') ){
+            $('.popups').removeClass('showing');
+            var dataName = target.attr('data');
+            console.log('dataName ', dataName)
+            $('.'+ dataName +'-popup').addClass('showing');
+        }
+        else if ( target.parent().hasClass('option') ){
+            $('.popups').removeClass('showing');
+            var dataName = target.parent().attr('data');
+            console.log('dataName ', dataName)
+            $('.'+ dataName +'-popup').addClass('showing');
+        }
+        else {
+            $('.popups').removeClass('showing');
+        }
+    })
+
+
   });
